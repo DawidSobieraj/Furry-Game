dd701226e1a395a3b5776cd7a2ca635f59d7fc64
Dawid Sobieraj <dawidsobieraj1992@gmail.com>
2018-03-25 19:17:30 +0200

    furry

---
+++ js/app.js
@@ -0,0 +1,108 @@
+function Furry() {
+    this.x = 0;
+    this.y = 0;
+    this.direction = "right";
+}
+
+function Coin() {
+    this.x = Math.floor(Math.random() * 10);
+    this.y = Math.floor(Math.random() * 10);
+}
+
+function Game() {
+    this.board = document.querySelectorAll("#board div");
+    this.furry = new Furry();
+    this.coin = new Coin();
+    this.score = 0;
+    this.idSetInterval = undefined;
+    this.showScore= document.querySelector('#score strong');
+
+    this.index = function (x, y) {
+        return x + (y * 10);
+    };
+
+    this.showFurry = function () {
+        this.hideVisibleFurry();
+        this.board[this.index(this.furry.x, this.furry.y)].classList.add('furry');
+    };
+
+    this.hideVisibleFurry = function () {
+        var oldFurry = document.querySelector(".furry");
+        if (oldFurry) {
+            oldFurry.classList.remove("furry");
+        }
+    };
+
+    this.showCoin = function () {
+        this.board[this.index(this.coin.x, this.coin.y)].classList.add("coin");
+    };
+
+    this.moveFurry = function () {
+
+        if (this.furry.direction === "right") {
+            this.furry.x = this.furry.x + 1;
+        } else if (this.furry.direction === "left") {
+            this.furry.x = this.furry.x - 1;
+        } else if (this.furry.direction === "up") {
+            this.furry.y = this.furry.y + 1;
+        } else if (this.furry.direction === "down") {
+            this.furry.y = this.furry.y - 1;
+        }
+
+        this.gameOver();
+        this.showFurry();
+        this.checkCoinCollision();
+
+    };
+
+    this.turnFurry = function () {
+        switch (event.which) {
+            case 37:
+                this.furry.direction = 'left';
+                break;
+            case 38:
+                this.furry.direction = 'down';
+                break;
+            case 39:
+                this.furry.direction = 'right';
+                break;
+            case 40:
+                this.furry.direction = 'up';
+                break;
+        }
+    };
+
+    this.checkCoinCollision = function () {
+        if ((this.furry.x === this.coin.x) && (this.furry.y === this.coin.y)) {
+            this.board[this.index(this.coin.x,this.coin.y)].classList.remove('coin');
+            this.score++;
+            this.showScore.textContent = this.score;
+            this.coin = new Coin();
+            this.showCoin();
+        }
+    };
+
+    this.gameOver = function () {
+        if ((this.furry.x < 0) || (this.furry.x > 9) || (this.furry.y < 0) || (this.furry.y > 9)) {
+            clearInterval(this.idSetInterval);
+            this.hideVisibleFurry();
+            alert("Przegrałeś :( Twój wynik to " + this.score);
+        }
+    };
+
+    this.startGame = function () {
+        var self = this;
+        this.idSetInterval = setInterval(function () {
+            self.moveFurry();
+        }, 250);
+    };
+}
+
+var game = new Game();
+
+game.startGame();
+game.showCoin();
+
+document.addEventListener('keydown', function(event) {
+    game.turnFurry(event);
+});
